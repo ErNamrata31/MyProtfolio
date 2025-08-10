52e5d23996b22f860faa36a7eee6b8c7db75f0b7
Namrata <ynamrata1950@gmail.com>
2025-08-10 16:45:38 +0530

    no change

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,18 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutusComponent } from './about/aboutus.component';
 import { SkillsComponent } from './skills/skills.component';
 import { ProjectComponent } from './project/project.component';
 
 export const routes: Routes = [
-    {path:  '',component:HomeComponent},
-    {path:  'home',component:HomeComponent},
-    {path:'about',component:AboutusComponent},
-    {path:'skills',component:SkillsComponent},
-    {path:'projects',component:ProjectComponent},
-    {path: 'home', redirectTo: '', pathMatch: 'full' }
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutusComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'projects', component: ProjectComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' } // Wildcard route for a 404 page
 ];
+
+
+
